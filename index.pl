c013c7d6a2cda7b131f0e9c27cd911dfa7067711
zhencheng.szc <zhencheng.szc@alibaba-inc.com>
2020-08-06 18:36:27 +0800

    修改存储地，兼容linux系统

--- index.pl
+++ index.pl
@@ -1,8 +1,5 @@
 #!/usr/bin/perl
 
-$TODODIR="/usr/local/Cellar/todo/1.0" ;
-$TODOFILE="${TODODIR}/todolist" ;
-
 ### 参数解释:
 sub paramDesc {
     my $desc = <<"EOF" ;
@@ -37,9 +34,16 @@ sub trim {
     return $ss ;
 }
 
+### 获得当前用户的home目录
+sub getHomePath {
+    my $home = `echo \$HOME`;
+    return trim($home);
+}
+
 ### 读取文件后n行
 # 参数1: 文件名
-# 参数2: n,如果不传则读取整个文件
+# 参数2: n
+## 返回: 读取的内容
 sub readTail {
     my $filename = $_[0] ;
     open(DATA, "<$filename") ;
@@ -50,13 +54,14 @@ sub readTail {
     if ($paramSize == 2) {
         $n = $_[1] ;
     }
-    if ($size < $n) {
-        $n = $size ;
-    }
-    for ($i = 1 ; $i <= $n ; $i = $i + 1) {
-        print  + "$i.@lines[$size - $n + $i - 1]" ;
+    $content = "";
+    for ($i = 1 ; $i <= $size ; $i = $i + 1) {
+        if ($i > $size - $n) {
+            $content = "${content}${i}.@lines[$i-1]";
+        }
     }
     close(DATA) ;
+    return $content;
 }
 
 ### 在文件里添加一条消息
@@ -70,6 +75,7 @@ sub addItem {
     open(DATA, ">>$filename");
     print DATA $item ;
     close(DATA) ;
+    return $item ;
 }
 
 ### 删除第n条
@@ -77,36 +83,32 @@ sub addItem {
 # 参数2: n,如果不传则删除最后一条
 sub del {
     $filename = @_[0] ;
-    my $n = 0 ;
-    my $size = @_ ;
-    if ($size > 1) {
+    my $paramSize = @_ ;
+    my $n = $paramSize ;
+    if ($paramSize > 1) {
         $n = @_[1] ;
     }
-    open(DATA, "<$filename");
-    my @lines = <DATA> ;
-    my $lineCount = @lines ;
-    if ($size == 1) {
-        $n = $lineCount ;
-    }
-    if ($n < 0 || $n > $lineCount) {
-        return ;
-    }
-    $content = "" ;
-    for ($i = 1 ; $i <= $lineCount ; $i = $i + 1) {
-        if ($i != $n) {
-            $content = "${content}@lines[$i - 1]" ;
-        }
-    }
-    close(DATA) ;
+    my $content = readTail($filename) ;
+    my @items = split('\n', $content) ;
+    my $size = @items ;
 
     open(DATA2, ">$filename") ;
-    print DATA2 $content ;
+    for ($i = 1 ; $i <= $size ; $i = $i + 1) {
+        if ($i != $n) {
+            my $item = @items[$i - 1] ;
+            $item =~ s/^([0-9]+\.)// ;
+            print DATA2 "$item\n" ;
+        }
+    }
     close(DATA2) ;
 }
 
 ### 初始化
 sub init {
     # 检查是否存在~/.todo/todolist文件,如果不存在则创建
+    $TODODIR = getHomePath() ;
+    $FILENAME = ".todolist" ;
+    $TODOFILE = "${TODODIR}/${FILENAME}" ;
     createDir($TODODIR) ;
     createFile($TODOFILE) ;
 }
@@ -115,7 +117,7 @@ sub main {
     init() ;
     my $paramSize = @_ ;
     if ($paramSize == 0) {
-        readTail($TODOFILE, 3) ;
+        print readTail($TODOFILE, 3) ;
         exit ;
     }
     my $firstParam = @_[0] ;
@@ -123,7 +125,7 @@ sub main {
     if ($firstParam =~ /^-[a-zA-Z]{1}$/) {
         $firstParam =~ s/-// ;
         if ($firstParam eq "a") {
-            readTail($TODOFILE) ;
+            print readTail($TODOFILE) ;
             exit ;
         } elsif ($firstParam eq "A") {
             if ($paramSize == 2) {
